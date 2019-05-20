e7b3e94ffc51561bdbe7e38a1ee3093dca34a00a
yaqoo <qiaoxiangxiang0123@163.com>
2019-05-20 12:35:58 +0800

    0520

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 // $("head").append(`
 //     <link rel="stylesheet" href="/css/common.css">
 // `)
-$(".header").append(`
+$(".headerIndex").append(`
     <div class="head">
         <div class="headTop">
             <div class="headTopC">
@@ -26,9 +26,9 @@ $(".header").append(`
                         <span class="headTopShu">|</span>
                         <a href="#"><span class="headToptxt1">买课</span></a>
                     </div>
-                    <a href="/course/shopCar.html">
+                    <a href="./html/course/shopCar.html">
                         <div class="headTopCRR">
-                            <img class="headTopCRRImg" src="/img/home/gouwuche.png" alt="">
+                            <span  class="headTopCRRImg iconfont">&#xe635;</span>
                             <span class="headTopCRRTxt">购物车</span>
                             <span class="headTopCRRNum">12</span>
                         </div>
@@ -39,8 +39,8 @@ $(".header").append(`
         <div class="headContent">
             <div class="headCT">
                 <div class="headCTLeft">
-                    <img class="headCTLogo" src="/img/home/logo.png" alt="">
-                    <img class="headCTDW" src="/img/home/dignwei.png" alt="">
+                    <img class="headCTLogo" src="./img/home/logo.png" alt="">
+                    <img class="headCTDW" src="./img/home/dignwei.png" alt="">
                 </div>
                 <div class="headCTIput">
                     <div class="headCTselect">
@@ -56,11 +56,11 @@ $(".header").append(`
                         <input type="text" placeholder="请输入关键词"/>
                     </div>
                     <div class="headCTsousuo">
-                        <img class="headCTsousuoImg" src="/img/home/sousuo.png" alt="">
+                        <span class="iconfont headCTsousuoImg">&#xe63c;</span>
                     </div>
                 </div>
                 <a href="#" class="headCTRight">
-                    <img class="headCTRImg" src="/img/home/app.png" alt="">
+                    <img class="headCTRImg" src="./img/home/app.png" alt="">
                     <h3 class="headCTH3">
                         <span class="headCTRtxt">官方APP</span><br/>
                         <span class="headCTRtxt1">在线学习</span>
@@ -69,21 +69,21 @@ $(".header").append(`
             </div>
             <div class="headCB">
                 <div class="headCBL">
-                    <div class="headCBLT"><img src="/img/home/liebioa.png">选课中心</div>
+                    <div class="headCBLT"><img src="./img/home/liebioa.png">选课中心</div>
                     <div class="headCBLlists">    
-                        <div class="headCBLlist"><a href="#">监管人员<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">餐饮领域学员<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">流通领域学员<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">生产领域学员<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">送餐人员动画课程<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">入网餐饮商户课程<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">食品安全动画课程<img src="/img/home/jiantou2.png"></a></div>
-                        <div class="headCBLlist"><a href="#">专家论坛<img src="/img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">监管人员<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">餐饮领域学员<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">流通领域学员<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">生产领域学员<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">送餐人员动画课程<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">入网餐饮商户课程<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">食品安全动画课程<img src="./img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">专家论坛<img src="./img/home/jiantou2.png"></a></div>
                     </div>
                 </div>
                 <div class="headCBC">
                     <span class="headCBCNav">
-                        <a href="/index.html">
+                        <a href="./index.html">
                             首页
                         <span class="headCBCXian"></span>
                     </a>
@@ -136,6 +136,187 @@ $(".header").append(`
     </div>
 `)
 
+$(".header").append(`
+    <div class="head">
+        <div class="headTop">
+            <div class="headTopC">
+                <div class="headTopCLeft">
+                    <a href="#"><span class="headToptxt">健康中国</span></a>
+                    <span class="headTopShu">|</span>
+                    <a href="#"><span class="headToptxt1">报刊</span></a>
+                    <span class="headTopShu">|</span>
+                    <a href="#"><span class="headToptxt1">出版</span></a>
+                    <span class="headTopShu">|</span>
+                    <a href="#"><span class="headToptxt1">资讯</span></a>
+                    <span class="headTopShu">|</span>
+                    <a href="#"><span class="headToptxt1">舆情</span></a>
+                    <span class="headTopShu">|</span>
+                    <a href="#"><span class="headToptxt1">培训</span></a>
+                </div>
+                <div class="headTopCRight">
+                    <div class="headTopCRtxt">
+                        <a href="#"><span class="headToptxt">登录</span></a>
+                        <span class="headTopShu">|</span>
+                        <a href="#"><span class="headToptxt1">注册</span></a>
+                        <span class="headTopShu">|</span>
+                        <a href="#"><span class="headToptxt1">买课</span></a>
+                    </div>
+                    <a href="./html/course/shopCar.html">
+                        <div class="headTopCRR">
+                            <span  class="headTopCRRImg iconfont">&#xe635;</span>
+                            <span class="headTopCRRTxt">购物车</span>
+                            <span class="headTopCRRNum">12</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="headContent">
+            <div class="headCT">
+                <div class="headCTLeft">
+                    <img class="headCTLogo" src="../../img/home/logo.png" alt="">
+                    <img class="headCTDW" src="../../img/home/dignwei.png" alt="">
+                </div>
+                <div class="headCTIput">
+                    <div class="headCTselect">
+                        <select class="headCTRight" name="" id="headSelect">
+                            <option value="">课程</option>
+                            <option value="">视频</option>
+                            <option value="">视频</option>
+                            <option value="">视频</option>
+                            <option value="">视频</option>
+                        </select>
+                    </div>
+                    <div class="headCTInput">
+                        <input type="text" placeholder="请输入关键词"/>
+                    </div>
+                    <div class="headCTsousuo">
+                        <span class="iconfont headCTsousuoImg">&#xe63c;</span>
+                    </div>
+                </div>
+                <a href="#" class="headCTRight">
+                    <img class="headCTRImg" src="../../img/home/app.png" alt="">
+                    <h3 class="headCTH3">
+                        <span class="headCTRtxt">官方APP</span><br/>
+                        <span class="headCTRtxt1">在线学习</span>
+                    </h3>
+                </a>
+            </div>
+            <div class="headCB">
+                <div class="headCBL">
+                    <div class="headCBLT"><img src="../../img/home/liebioa.png">选课中心</div>
+                    <div class="headCBLlists">    
+                        <div class="headCBLlist"><a href="#">监管人员<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">餐饮领域学员<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">流通领域学员<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">生产领域学员<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">送餐人员动画课程<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">入网餐饮商户课程<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">食品安全动画课程<img src="../../img/home/jiantou2.png"></a></div>
+                        <div class="headCBLlist"><a href="#">专家论坛<img src="../../img/home/jiantou2.png"></a></div>
+                    </div>
+                </div>
+                <div class="headCBC">
+                    <span class="headCBCNav">
+                        <a href="../../index.html">
+                            首页
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            新闻资讯
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            在线考场
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            法规标准
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            师资力量
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            操作助手
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                    <span class="headCBCNav">
+                        <a href="#">
+                            关于我们
+                        <span class="headCBCXian"></span>
+                    </a>
+                    </span>
+                </div>
+                <div class="headCBR">
+                    <a href="#">
+                        App下载
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+`)
+
+
+$(".footerIndex").append(`
+    <div class="foot">
+        <div class="footC">
+            <div class="footCTitle">
+                友情链接
+            </div>
+            <div class="footCNav">
+                <span><a href="#"> 腾讯教育</span></a>
+                <span><a href="#">Android开发教程</span></a>
+                <span><a href="#">新疆人才网</span></a>
+                <span><a href="#">教育培训</span></a>
+                <span><a href="#">hao123</span></a>
+                <span><a href="#">分类信息</span></a>
+                <span><a href="#">创业网</span></a>
+                <span><a href="#">网易直播</span></a>
+                <span><a href="#">新浪商学院</span></a>
+                <span><a href="#">28推论坛</span></a>
+                <span><a href="#">优胜教育</span></a>
+                <span><a href="#">编织人生</span></a>
+                <span><a href="#">薛掌柜</span></a>
+                <span><a href="#">CFA</span></a>
+                <span class="footCNavB"><a href="#">linux</span></a>
+                <span><a href="#">创业论坛</span></a>
+                <span><a href="#">甘肃公务员考试网</span></a>
+                <span><a href="#">北京兼职</span></a>
+            </div>
+            <div class="footCbottom">
+                <div class="footCbottomL">
+                    <img src="./img/home/footImg.png" alt="">
+                    <div class="footCbottomTxt">
+                        <div class="footCbottomTxt1">食安在线版权所有</div>
+                        <div>ICP备案编号： 京ICP备18001302号-2 | 京公网安备11010802023718号</div>
+                        <div>Copyright © 2016 - 2018 CMSTP Co.,Ltd All Rights Reserved.</div>
+                    </div>
+                </div>
+                <div class="footCbottomR">
+                    <div class="footCbottomTxt2">400-020-1688</div>
+                    <div>周一至周五 9:00 - 21:00</div>
+                    <div>( 全国免费咨询 )</div>
+                </div>
+            </div>
+        </div>
+    </div>
+`)
+
 $(".footer").append(`
     <div class="foot">
         <div class="footC">
@@ -164,7 +345,7 @@ $(".footer").append(`
             </div>
             <div class="footCbottom">
                 <div class="footCbottomL">
-                    <img src="/img/home/footImg.png" alt="">
+                    <img src="../../img/home/footImg.png" alt="">
                     <div class="footCbottomTxt">
                         <div class="footCbottomTxt1">食安在线版权所有</div>
                         <div>ICP备案编号： 京ICP备18001302号-2 | 京公网安备11010802023718号</div>
@@ -180,6 +361,8 @@ $(".footer").append(`
         </div>
     </div>
 `)
+
+
 $(".contentLeft").append(`
 <div class="content_left_top">
     <a href="#" class="contentLTli">
@@ -291,29 +474,3 @@ function countDown_H_M_S(timehtml) {
 }
 
 
-//本地观看 带有域名可删除
-var num = window.location.href.indexOf("shianzaixian")+12;
-var url = window.location.href;
-console.log();
-if(url.substring(num+1,num+6) == "index"){
-	for(var i=0;i<$(".header").find("img").length;i++){
-		$(".header").find("img").eq(i).attr("src","."+$(".header").find("img").eq(i).attr("src"))
-	}
-	for(var i=0;i<$(".footer").find("img").length;i++){
-		$(".footer").find("img").eq(i).attr("src","."+$(".footer").find("img").eq(i).attr("src"))
-	}
-	for(var i=0;i<$(".contentLeft").find("img").length;i++){
-		$(".contentLeft").find("img").eq(i).attr("src","."+$(".contentLeft").find("img").eq(i).attr("src"))
-	}
-//	$(".header").find("img").attr("src","."+$(".header").find("img").attr("src"))
-}else{
-	for(var i=0;i<$(".header").find("img").length;i++){
-		$(".header").find("img").eq(i).attr("src","../.."+$(".header").find("img").eq(i).attr("src"))
-	}
-	for(var i=0;i<$(".footer").find("img").length;i++){
-		$(".footer").find("img").eq(i).attr("src","../.."+$(".footer").find("img").eq(i).attr("src"))
-	}
-	for(var i=0;i<$(".contentLeft").find("img").length;i++){
-		$(".contentLeft").find("img").eq(i).attr("src","../.."+$(".contentLeft").find("img").eq(i).attr("src"))
-	}
-}
